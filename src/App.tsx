71d5b2463886461c8ddfc17bd3f4a6a0dde58ed3
jdpena28 <jhmdelapena@bpsu.edu.ph>
2021-10-21 08:55:34 +0800

    Refactor code on async call for historical data

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,50 @@ import Loaders from './components/Loaders';
 
 
 const App: React.FC = () => {
-  const [data, SetCovidData] = useState<covidData>()
+  const [data, setCovidData] = useState<covidData>()
+  const [historical,setHistorical] = useState<any>({})
   const [loading,setLoading] = useState<boolean>(true)
 
   const getData = async() => {
     await axios.get('https://disease.sh/v3/covid-19/countries/philippines?strict=true')
     .then((response) => {
-        SetCovidData(response.data);
+        setCovidData(response.data);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
+  const buildChartData = (data?:any, casesType?:any) => {
+    let chartData = [];
+    let lastDataPoint;
+  
+    for (let date in data.cases) {
+      if (lastDataPoint) {
+        let newDataPoint = {
+          x: date,
+          y: data[casesType][date] - lastDataPoint,
+        };
+        chartData.push(newDataPoint);
+      }
+      lastDataPoint = data[casesType][date];
+    }
+    return chartData;
+  };
+
+  const fetchData = async () => {
+    await fetch("https://disease.sh/v3/covid-19/historical/phl?lastdays=60")
+      .then((response) => response.json())
+      .then((data) => {
+        let chartData = buildChartData(data.timeline, 'cases');
+        setHistorical(chartData);
+      })
+}
+
 
   useEffect(() => {
-    getData();
+    getData()
+    fetchData()
     setTimeout(() => {
       setLoading(false)
     },900)
@@ -58,19 +86,21 @@ const App: React.FC = () => {
     <div className="relative w-full bg-gray-900 text-gray-200 min-h-screen space-y-3 sm:pb-11">
       <Header />
     { loading?(<Loaders loading={loading}/>)
-    :(<><div className="container mx-auto px-20 grid grid-cols-12 gap-x-4 sm:flex sm:flex-col sm:px-6 sm: gap-y-5">
-        <Today
-          todayCases={format(data?.todayCases)}
-          todayDeaths={format(data?.todayDeaths)}
-          todayRecovered={format(data?.todayRecovered)}
-        />
-        <Total
-          active={format(data?.active)}
-          critical={format(data?.critical)}
-          deaths={format(data?.deaths)}
-        />
-      </div>
-      <LineGraph/></>)}
+    :(<>
+      <div className="container mx-auto px-20 grid grid-cols-12 gap-x-4 sm:flex sm:flex-col sm:px-6 sm: gap-y-5">
+          <Today
+            todayCases={format(data?.todayCases)}
+            todayDeaths={format(data?.todayDeaths)}
+            todayRecovered={format(data?.todayRecovered)}
+          />
+          <Total
+            active={format(data?.active)}
+            critical={format(data?.critical)}
+            deaths={format(data?.deaths)}
+          />
+        </div>
+        <LineGraph historicalData = {historical}/>
+      </>)}
       <Footer />
     </div>
     )
--- src/components/LineGraph.tsx
+++ src/components/LineGraph.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 
-
-
 const options:{} = {
   legend: {
     display: true,
@@ -13,61 +11,23 @@ const options:{} = {
     },
   },
   maintainAspectRatio: false,
-  scales: {
-    xAxes: [
-      {
-        
-      },
-    ],
-    yAxes: [
-      {
-        
-      },
-    ],
-  },
-};
-const buildChartData = (data?:any, casesType?:any) => {
-  let chartData = [];
-  let lastDataPoint;
-
-  for (let date in data.cases) {
-    if (lastDataPoint) {
-      let newDataPoint = {
-        x: date,
-        y: data[casesType][date] - lastDataPoint,
-      };
-      chartData.push(newDataPoint);
-    }
-    lastDataPoint = data[casesType][date];
-  }
-
-  return chartData;
 };
 
-const LineGraph = () => {
-  const [data, setData] = useState<any>({});
-  useEffect(() => {
-    const fetchData = async () => {
-      await fetch("https://disease.sh/v3/covid-19/historical/phl?lastdays=60")
-        .then((response) => response.json())
-        .then((data) => {
-          let chartData = buildChartData(data.timeline, 'cases');
-          setData(chartData);
-        })
-    }
-    fetchData()
-  }, [])
 
+
+  
+const LineGraph = ({historicalData}:{historicalData:any}) => {
+  
   return (
     <div className='h-[46vh]'>
-      {data.length > 0 && (
+      {historicalData.length > 0 && (
         <Line
           data={{
             datasets: [
               {
                 label:'Covid Trends in Last 60 days',
                 borderColor: "#9a32ef",
-                data: data,
+                data: historicalData,
               },
             ],
           }}
